a26b6e6347faa48dfd60e52a26f2794b5279a9af
Ariel D'Alessandro <ariel@vanguardiasur.com.ar>
2019-06-08 16:15:11 -0300

    stm32-f3: syscfg: Add SYSCFG_CFGR1 memory mapping bits
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- include/libopencm3/stm32/f3/syscfg.h
+++ include/libopencm3/stm32/f3/syscfg.h
@@ -35,7 +35,49 @@
 
 #ifndef LIBOPENCM3_SYSCFG_H
 #define LIBOPENCM3_SYSCFG_H
+/**@{*/
 
 #include <libopencm3/stm32/common/syscfg_common_l1f234.h>
+
+/*****************************************************************************/
+/* Module definitions                                                        */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* Register definitions                                                      */
+/*****************************************************************************/
+
+#define SYSCFG_CFGR1			MMIO32(SYSCFG_BASE + 0x00)
+#define SYSCFG_EXTICR(i)		MMIO32(SYSCFG_BASE + 0x08 + (i)*4)
+#define SYSCFG_EXTICR1			SYSCFG_EXTICR(0)
+#define SYSCFG_EXTICR2			SYSCFG_EXTICR(1)
+#define SYSCFG_EXTICR3			SYSCFG_EXTICR(2)
+#define SYSCFG_EXTICR4			SYSCFG_EXTICR(3)
+#define SYSCFG_CFGR2			MMIO32(SYSCFG_BASE + 0x18)
+
+/*****************************************************************************/
+/* Register values                                                           */
+/*****************************************************************************/
+
+/* SYSCFG_CFGR1 Values -- ---------------------------------------------------*/
+
+#define SYSCFG_CFGR1_MEM_MODE_SHIFT		0
+#define SYSCFG_CFGR1_MEM_MODE		(3 << SYSCFG_CFGR1_MEM_MODE_SHIFT)
+#define SYSCFG_CFGR1_MEM_MODE_FLASH	(0 << SYSCFG_CFGR1_MEM_MODE_SHIFT)
+#define SYSCFG_CFGR1_MEM_MODE_SYSTEM	(1 << SYSCFG_CFGR1_MEM_MODE_SHIFT)
+#define SYSCFG_CFGR1_MEM_MODE_SRAM	(3 << SYSCFG_CFGR1_MEM_MODE_SHIFT)
+
+/*****************************************************************************/
+/* API definitions                                                           */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* API Functions                                                             */
+/*****************************************************************************/
+
+BEGIN_DECLS
+
+END_DECLS
+/**@}*/
 
 #endif
